52db0769731bd448e8d8996cf25a1373ad2bc67c
Carricossauro <tiagocarrico01@gmail.com>
2024-04-02 21:06:08 +0200

    concession final step

--- Code_for_assignment/group1/helpers/runner.py
+++ Code_for_assignment/group1/helpers/runner.py
@@ -72,7 +72,7 @@ def run_a_tournament(
             n_repetitions=1,
             njobs=-1 if debug else 0,
             verbosity=2 if debug else 1,
-            plot_fraction=.5,
+            plot_fraction=0,
             name=name,
         ).final_scores
     print(f"Finished in {humanize_time(time.perf_counter() - start)}")
